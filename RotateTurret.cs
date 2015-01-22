24f9c96192a3c82297f10b5c18112aec9e429d8e
emre40 <emree_40_1997@live.nl>
2015-01-22 10:00:37 +0100

    Create RotateTurret.cs

---
+++ RotateTurret.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class RotateTurret : BaseRotateTurret {
+
+    public Camera camera;
+
+	override protected void Update () {
+        Vector3 mousePos = Input.mousePosition; //(x,y,z)
+        mousePos.z = camera.transform.position.y - turret.transform.position.y;
+
+        Vector3 worldPos = camera.ScreenToWorldPoint(mousePos);
+       
+        targetPos = worldPos;
+        base.Update();
+	
+	}
+}
